588d2d566149a368e5767a7e9718bc2c6cfe4b84
luzdarygarciapaz <luzdarygarciapaz@gmail.com>
2022-05-13 16:40:54 -0500

    actualizado 13-05-22

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const server = express();
 const port = 4000;
 
-server.get('/', (request, response)=>{
-    response.send('Hola desde la raiz'); // peticion al puerto 4000
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('Conectado a la base de datos'))
+  .catch((err) =>
+    console.log('Error al conectar a la base de datos, error: ' + err)
+  );
+
+server.get('/', (request, response) => {
+  response.send('Hola desde la raíz');
 });
 
-server.listen(port, ()=>{
-    console.log('servidor corriendo en el puerto ${port}')}); // callback funcion servidor correindo en el local host
+server.listen(port, () => {
+  console.log(`Servidor corriendo en localhost, en el puerto ${port}`);
+});
