205d93f35b79e9b9ee4b96e7ae27849350ddc05f
luzdarygarciapaz <luzdarygarciapaz@gmail.com>
2022-05-14 17:15:44 -0500

    Base de datos OK

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,11 +8,9 @@ const port = 4000;
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log('Conectado a la base de datos'))
-  .catch((err) =>
-    console.log('Error al conectar a la base de datos, error: ' + err)
-  );
+.connect(process.env.MONGODB_URI)
+.then(() => console.log('conectado a la base datos')) 
+.catch((err) => console.log('error al conectar a la base de datos error: ' + err));
 
 server.get('/', (request, response) => {
   response.send('Hola desde la raíz');
